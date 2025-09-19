2e17eba09eb4f4d0056ff5237547f91e3e5ae375
matiasjaravega <mjarav@anticipa.cl>
2025-09-18 21:50:52 -0300

    Inicio Spring Boot

---
+++ sistema-dideco/src/main/java/cl/municipalidadchillan/dideco/SistemaDidecoApplication.java
@@ -0,0 +1,13 @@
+package cl.municipalidadchillan.dideco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDidecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDidecoApplication.class, args);
+	}
+
+}
